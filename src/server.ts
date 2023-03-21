bf6d9e3d00f325db46a569d1d76a684dbd012f81
Karina Mariano <kmariano138@gmail.com>
2023-03-21 10:48:04 -0300

    create routes get and post user

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,14 @@ const app = express();
 
 app.use(express.json());
 
+//rota pegar de categoria 
 app.get('/category', async (req, res) => {
     const categories = await prisma.category.findMany();
 
     return res.json(categories)
 });
 
+//rota postar de categoria
 app.post('/category', async (req, res) => {
     const { title } = req.body;
 
@@ -23,6 +25,7 @@ app.post('/category', async (req, res) => {
     return res.json(category)
 });
 
+//rota pegar de aula
 app.get('/classroom', async (req, res) => {
     const classes =  await prisma.classroom.findMany({
         include: {
@@ -33,6 +36,7 @@ app.get('/classroom', async (req, res) => {
     return res.json(classes)
 });
 
+//rota postar de aula
 app.post('/classroom', async (req, res) => {
     const { title, numberClasses,time,video,description,categoryId } = req.body;
 
@@ -50,7 +54,36 @@ app.post('/classroom', async (req, res) => {
     return res.json(classroom)
 });
 
+//rota pegar de usuario
+app.get('/user', async (req, res) => {
+    const users =  await prisma.classroom.findMany(
+    //     {
+    //     include: {
+    //         category: true
+    //     }
+    // } acho que nao precisa disso
+    );
 
+    return res.json(users)
+});
+
+//rota postar de usuario
+app.post('/user', async (req, res) => {
+    const {idDevice} = req.body;
+
+    const users = await prisma.classroom.create({
+        data: { 
+        idDevice // nao to entendendo pq ta dando o erro, fala que nao é permitido coolocar ele aqui
+    }
+    })
+
+    return res.json(users)
+});
+
+//rota pegar de categoria usuario
+//rota postar de categoria usuario
+//rota pegar de aula usuario
+//rota postar dee aula usuario
 
 
 app.listen(3333, () => {
